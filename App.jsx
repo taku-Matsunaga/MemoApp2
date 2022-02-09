ce68d863b0428530ad0ce9dcab96d625a16b87c4
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-09 15:28:21 +0900

    replace avoiding view

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    // <MemoEditScreen />
-    <MemoCreateScreen />
+    <MemoEditScreen />
+    // <MemoCreateScreen />
   );
 }

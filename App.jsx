5ebd05c5d1b7134c26b7cc5a7ee4866c28d8ad9c
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-09 16:22:12 +0900

    create login and signup screen

--- App.jsx
+++ App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import SignUpScreen from './src/screens/SignUpScreen';
+// import LoginScreen from './src/screens/LoginScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
     // <MemoCreateScreen />
+    // <LoginScreen />
+    <SignUpScreen />
   );
 }

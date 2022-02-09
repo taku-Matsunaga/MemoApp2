c9be3ad0db545a791bb39baf58f1cc5bc89a3059
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-09 15:13:42 +0900

    memo screen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
-    <MemoDetailScreen />
+    // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }

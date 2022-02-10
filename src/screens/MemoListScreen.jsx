ff03b6adaaa9873bdeb1f7a7ff31cbf42bb50ecf
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-10 16:36:20 +0900

    get data from firebase

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,5 +1,6 @@
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import React, { useEffect } from 'react';
+import firebase from 'firebase';
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
@@ -12,6 +13,25 @@ export default function MemoListScreen(props) {
       headerRight: () => <LogOutButton />,
     });
   }, []);
+
+  useEffect(() => {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => { };
+    if (currentUser) {
+      const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'dest');
+      unsubscribe = ref.onSnapshot((snapshot) => {
+        snapshot.forEach((doc) => {
+          console.log(doc.id, doc.data());
+        });
+      }, (error) => {
+        console.log(error);
+        Alert.alert('データの読み込みに失敗しました');
+      });
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <MemoList />

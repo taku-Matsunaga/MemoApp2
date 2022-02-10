f8c8da6e4acaaf83aac5076c2fcc56877dd9c45f
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-10 16:07:57 +0900

    storage memo

--- App.jsx
+++ App.jsx
@@ -11,11 +11,13 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 import { firebaseConfig } from './env';
 
-const Stack = createStackNavigator();
+require('firebase/firestore');
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
